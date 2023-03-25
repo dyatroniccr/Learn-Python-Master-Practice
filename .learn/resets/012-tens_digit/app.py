360b9d3fd49f680d4d5e805fd62da547b693cf26
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-25 07:05:00 +0000

    Update ej14

--- .learn/resets/012-tens_digit/app.py
+++ .learn/resets/012-tens_digit/app.py
@@ -1,11 +1,9 @@
 #Complete the function to return the tens digit of a given interger
 def tens_digit(num):
-  centenas = int(num)%100
-  decenas = centenas //10
-  return decenas
+  return None
 
 
 
 
 #Invoke the function with any interger.
-print(tens_digit(11459))
+print(tens_digit())
--- .learn/resets/013-sum_of_digits/app.py
+++ .learn/resets/013-sum_of_digits/app.py
@@ -1,11 +1,6 @@
 #Complete the function "digits_sum" so that it prints the sum of a three digit number.
 def digits_sum(num):
-  centenas = int(num) // 100
-  d_temp = int(num)%100
-  decenas = d_temp //10
-  unid = d_temp % 10
-  sum = centenas + decenas + unid
-  return sum
+  return None
 
 
 #Invoke the function with any three-digit-number
